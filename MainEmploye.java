46c9472a3b161b1560296665298537dec5471e65
Atman Boulaajaili <60273155+Elon-Fask@users.noreply.github.com>
2022-05-20 23:18:07 +0100

    Update MainEmploye.java

--- MainEmploye.java
+++ MainEmploye.java
@@ -7,19 +7,9 @@ public class MainEmploye {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String nm,ln,d;
-		double s, nmV,nmH,cms ;
+		double s,nmV,nmH,cms,tariF;
 		
-		
-		
-		/*Employe[] em = new Employe[4];
-		
-		em[0]= new EmployeeFix("max","smkith", "12/03/1990", 3000);
-		em[1]= new EmployeCommission("jeny", "lark","30/04/1989", 1000, 20, 0.5);
-		em[2]= new EmployeHoraire("maynard", "jaher", "20/12/1987", 20, 30);
-		
-		for(int i = 0; i> em.length;i++ ) {
-              em[i].ShowData();
-	}*/
+		// insert your data 		
 		System.out.println("Enter ur  first name");
 		nm =sc.nextLine();
 		System.out.println("Enter ur  last name");
@@ -28,34 +18,45 @@ public class MainEmploye {
 		d =sc.nextLine();
 		
 		
-		
-		
-		String ef = "Atman"; //employeeFix
-		String ec = "Maryam"; //employeeCommission
-		String eh = "Elon"; //employeeHour
+		String ef = "Atman", efl = "Boulaajaili"; //employeeFix ==== ef=empoyeefix ==== efl= employee last name 
+		String ec = "Maryam", ecl = "Blossom"; //employeeCommission ==== ecl= employee last name 
+		String eh = "Elon", ehl = "Musk";//employeeHour ===== ehl= employee last name 
 
 		
-		if ( nm.equals(ef) ) {
+		
+		// Calling the salary of the fixed Employee Salary :
+		if ( nm.equals(ef) && ln.equals(efl) ) {
 			EmployeeFix emF = new EmployeeFix(nm, ln, d,2000); 	
 			System.out.println(emF);
 			
-		} else if(nm.equals(ec)) {
-			System.out.println("Enter sales number : ");
-			nmV =sc.nextDouble();
-			EmployeCommission emC = new EmployeCommission(nm, ln, d, 1000, nmV, 0.5); 
-			System.out.println(emC);
-			
-		}
-		if (nm.equals(eh)) {
+		
+		//	Calling the Salary of the Employee with Commission :  
+		} else if(nm.equals(ec)&& ln.equals(ecl)) {
+				System.out.println("Enter sales number : ");
+				nmV =sc.nextDouble();
+				EmployeCommission emC = new EmployeCommission(nm, ln, d, 1000, nmV, 0.5); 
+				System.out.println(emC);
+			}
+
+		// Calling the Salary of the worked Hour Employee :
+		else if (nm.equals(eh) && ln.equals(ehl)) {
 			System.out.println("Enter Worked Hours : ");
 			nmH =sc.nextDouble();
-			EmployeHoraire emH = new EmployeHoraire(nm, ln, d, nmH, 50); 
+			System.out.println("Enter your Tarif : ");
+			tariF =sc.nextDouble();
+			EmployeHoraire emH = new EmployeHoraire(nm, ln, d, nmH, tariF); 
 			System.out.println(emH);
-
+			if (nmH>200 ) {
+				System.out.println("Worked Hour must be inferior than 200h !!!");
+			} 
+			else if (tariF < 20) {
+					System.out.println(" Tarif must be superior than 20dh !!!");
+				}
+		} else {
+			System.out.println("Please Your Name does not exist and/or your birthday dosen't match !!!!");
 		}
 		
-		
-		
+				
 		sc.close();
 }
 }
